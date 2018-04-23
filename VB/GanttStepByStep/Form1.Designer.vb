3c21568b27265a7d3b0351d175d6e4577cb672fe
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/GanttStepByStep/Form1.Designer.vb
@@ -0,0 +1,208 @@
+﻿Namespace GanttStepByStep
+    Partial Public Class Form1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        #Region "Windows Form Designer generated code"
+
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Dim timeRuler1 As New DevExpress.XtraScheduler.TimeRuler()
+            Dim timeRuler2 As New DevExpress.XtraScheduler.TimeRuler()
+            Me.schedulerStorage1 = New DevExpress.XtraScheduler.SchedulerStorage(Me.components)
+            Me.taskDependenciesBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+            Me.gantTestDataSet = New GanttStepByStep.GantTestDataSet()
+            Me.appointmentsBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+            Me.resourcesBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+            Me.splitContainerControl1 = New DevExpress.XtraEditors.SplitContainerControl()
+            Me.resourcesTree1 = New DevExpress.XtraScheduler.UI.ResourcesTree()
+            Me.colId = New DevExpress.XtraScheduler.Native.ResourceTreeColumn()
+            Me.colDescription = New DevExpress.XtraScheduler.Native.ResourceTreeColumn()
+            Me.schedulerControl1 = New DevExpress.XtraScheduler.SchedulerControl()
+            Me.appointmentsTableAdapter = New GanttStepByStep.GantTestDataSetTableAdapters.AppointmentsTableAdapter()
+            Me.resourcesTableAdapter = New GanttStepByStep.GantTestDataSetTableAdapters.ResourcesTableAdapter()
+            Me.taskDependenciesTableAdapter = New GanttStepByStep.GantTestDataSetTableAdapters.TaskDependenciesTableAdapter()
+            DirectCast(Me.schedulerStorage1, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(Me.taskDependenciesBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(Me.gantTestDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(Me.appointmentsBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(Me.resourcesBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(Me.splitContainerControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.splitContainerControl1.SuspendLayout()
+            DirectCast(Me.resourcesTree1, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' schedulerStorage1
+            ' 
+            Me.schedulerStorage1.AppointmentDependencies.DataSource = Me.taskDependenciesBindingSource
+            Me.schedulerStorage1.AppointmentDependencies.Mappings.DependentId = "DependentId"
+            Me.schedulerStorage1.AppointmentDependencies.Mappings.ParentId = "ParentId"
+            Me.schedulerStorage1.AppointmentDependencies.Mappings.Type = "Type"
+            Me.schedulerStorage1.Appointments.DataSource = Me.appointmentsBindingSource
+            Me.schedulerStorage1.Appointments.Mappings.AllDay = "AllDay"
+            Me.schedulerStorage1.Appointments.Mappings.AppointmentId = "UniqueId"
+            Me.schedulerStorage1.Appointments.Mappings.Description = "Description"
+            Me.schedulerStorage1.Appointments.Mappings.End = "EndDate"
+            Me.schedulerStorage1.Appointments.Mappings.Label = "Label"
+            Me.schedulerStorage1.Appointments.Mappings.Location = "Location"
+            Me.schedulerStorage1.Appointments.Mappings.PercentComplete = "PercentComplete"
+            Me.schedulerStorage1.Appointments.Mappings.RecurrenceInfo = "RecurrenceInfo"
+            Me.schedulerStorage1.Appointments.Mappings.ReminderInfo = "ReminderInfo"
+            Me.schedulerStorage1.Appointments.Mappings.ResourceId = "ResourceId"
+            Me.schedulerStorage1.Appointments.Mappings.Start = "StartDate"
+            Me.schedulerStorage1.Appointments.Mappings.Status = "Status"
+            Me.schedulerStorage1.Appointments.Mappings.Subject = "Subject"
+            Me.schedulerStorage1.Appointments.Mappings.Type = "Type"
+            Me.schedulerStorage1.Resources.CustomFieldMappings.Add(New DevExpress.XtraScheduler.ResourceCustomFieldMapping("CustomField1", "CustomField1"))
+            Me.schedulerStorage1.Resources.CustomFieldMappings.Add(New DevExpress.XtraScheduler.ResourceCustomFieldMapping("IdSort", "IdSort"))
+            Me.schedulerStorage1.Resources.DataSource = Me.resourcesBindingSource
+            Me.schedulerStorage1.Resources.Mappings.Caption = "Description"
+            Me.schedulerStorage1.Resources.Mappings.Color = "Color"
+            Me.schedulerStorage1.Resources.Mappings.Id = "Id"
+            Me.schedulerStorage1.Resources.Mappings.Image = "Image"
+            Me.schedulerStorage1.Resources.Mappings.ParentId = "ParentId"
+            ' 
+            ' taskDependenciesBindingSource
+            ' 
+            Me.taskDependenciesBindingSource.DataMember = "TaskDependencies"
+            Me.taskDependenciesBindingSource.DataSource = Me.gantTestDataSet
+            ' 
+            ' gantTestDataSet
+            ' 
+            Me.gantTestDataSet.DataSetName = "GantTestDataSet"
+            Me.gantTestDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+            ' 
+            ' appointmentsBindingSource
+            ' 
+            Me.appointmentsBindingSource.DataMember = "Appointments"
+            Me.appointmentsBindingSource.DataSource = Me.gantTestDataSet
+            ' 
+            ' resourcesBindingSource
+            ' 
+            Me.resourcesBindingSource.DataMember = "Resources"
+            Me.resourcesBindingSource.DataSource = Me.gantTestDataSet
+            ' 
+            ' splitContainerControl1
+            ' 
+            Me.splitContainerControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.splitContainerControl1.Location = New System.Drawing.Point(0, 0)
+            Me.splitContainerControl1.Name = "splitContainerControl1"
+            Me.splitContainerControl1.Panel1.Controls.Add(Me.resourcesTree1)
+            Me.splitContainerControl1.Panel1.Text = "Panel1"
+            Me.splitContainerControl1.Panel2.Controls.Add(Me.schedulerControl1)
+            Me.splitContainerControl1.Panel2.Text = "Panel2"
+            Me.splitContainerControl1.Size = New System.Drawing.Size(707, 450)
+            Me.splitContainerControl1.SplitterPosition = 202
+            Me.splitContainerControl1.TabIndex = 0
+            Me.splitContainerControl1.Text = "splitContainerControl1"
+            ' 
+            ' resourcesTree1
+            ' 
+            Me.resourcesTree1.Columns.AddRange(New DevExpress.XtraTreeList.Columns.TreeListColumn() { Me.colId, Me.colDescription})
+            Me.resourcesTree1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.resourcesTree1.Location = New System.Drawing.Point(0, 0)
+            Me.resourcesTree1.Name = "resourcesTree1"
+            Me.resourcesTree1.SchedulerControl = Me.schedulerControl1
+            Me.resourcesTree1.Size = New System.Drawing.Size(202, 450)
+            Me.resourcesTree1.TabIndex = 0
+            ' 
+            ' colId
+            ' 
+            Me.colId.FieldName = "Id"
+            Me.colId.Name = "colId"
+            ' 
+            ' colDescription
+            ' 
+            Me.colDescription.FieldName = "Description"
+            Me.colDescription.Name = "colDescription"
+            Me.colDescription.Visible = True
+            Me.colDescription.VisibleIndex = 0
+            ' 
+            ' schedulerControl1
+            ' 
+            Me.schedulerControl1.ActiveViewType = DevExpress.XtraScheduler.SchedulerViewType.Gantt
+            Me.schedulerControl1.DataStorage = Me.schedulerStorage1
+            Me.schedulerControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.schedulerControl1.Location = New System.Drawing.Point(0, 0)
+            Me.schedulerControl1.Name = "schedulerControl1"
+            Me.schedulerControl1.Size = New System.Drawing.Size(493, 450)
+            Me.schedulerControl1.Start = New Date(2016, 11, 8, 0, 0, 0, 0)
+            Me.schedulerControl1.TabIndex = 0
+            Me.schedulerControl1.Text = "schedulerControl1"
+            Me.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler1)
+            Me.schedulerControl1.Views.FullWeekView.Enabled = True
+            Me.schedulerControl1.Views.GanttView.ShowResourceHeaders = False
+            Me.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler2)
+            ' 
+            ' appointmentsTableAdapter
+            ' 
+            Me.appointmentsTableAdapter.ClearBeforeFill = True
+            ' 
+            ' resourcesTableAdapter
+            ' 
+            Me.resourcesTableAdapter.ClearBeforeFill = True
+            ' 
+            ' taskDependenciesTableAdapter
+            ' 
+            Me.taskDependenciesTableAdapter.ClearBeforeFill = True
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(707, 450)
+            Me.Controls.Add(Me.splitContainerControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            DirectCast(Me.schedulerStorage1, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(Me.taskDependenciesBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(Me.gantTestDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(Me.appointmentsBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(Me.resourcesBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(Me.splitContainerControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.splitContainerControl1.ResumeLayout(False)
+            DirectCast(Me.resourcesTree1, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+
+        End Sub
+
+        #End Region
+
+        Private schedulerStorage1 As DevExpress.XtraScheduler.SchedulerStorage
+        Private splitContainerControl1 As DevExpress.XtraEditors.SplitContainerControl
+        Private resourcesTree1 As DevExpress.XtraScheduler.UI.ResourcesTree
+        Private colId As DevExpress.XtraScheduler.Native.ResourceTreeColumn
+        Private colDescription As DevExpress.XtraScheduler.Native.ResourceTreeColumn
+        Private schedulerControl1 As DevExpress.XtraScheduler.SchedulerControl
+        Private gantTestDataSet As GantTestDataSet
+        Private appointmentsBindingSource As System.Windows.Forms.BindingSource
+        Private appointmentsTableAdapter As GantTestDataSetTableAdapters.AppointmentsTableAdapter
+        Private resourcesBindingSource As System.Windows.Forms.BindingSource
+        Private resourcesTableAdapter As GantTestDataSetTableAdapters.ResourcesTableAdapter
+        Private taskDependenciesBindingSource As System.Windows.Forms.BindingSource
+        Private taskDependenciesTableAdapter As GantTestDataSetTableAdapters.TaskDependenciesTableAdapter
+
+
+
+    End Class
+End Namespace
+
