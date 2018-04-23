d428876aef0ff5dc3545b24a10f485a10e9457cf
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/GanttExample/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace GanttExample {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraScheduler.TimeRuler timeRuler4 = new DevExpress.XtraScheduler.TimeRuler();
+            DevExpress.XtraScheduler.TimeRuler timeRuler5 = new DevExpress.XtraScheduler.TimeRuler();
+            DevExpress.XtraScheduler.TimeRuler timeRuler6 = new DevExpress.XtraScheduler.TimeRuler();
+            this.splitContainerControl1 = new DevExpress.XtraEditors.SplitContainerControl();
+            this.resourcesTree1 = new DevExpress.XtraScheduler.UI.ResourcesTree();
+            this.schedulerControl1 = new DevExpress.XtraScheduler.SchedulerControl();
+            this.schedulerStorage1 = new DevExpress.XtraScheduler.SchedulerStorage(this.components);
+            this.taskDependenciesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.gantTestDataSet = new GanttExample.GantTestDataSet();
+            this.appointmentsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.resourcesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.appointmentsTableAdapter = new GanttExample.GantTestDataSetTableAdapters.AppointmentsTableAdapter();
+            this.resourcesTableAdapter = new GanttExample.GantTestDataSetTableAdapters.ResourcesTableAdapter();
+            this.taskDependenciesTableAdapter = new GanttExample.GantTestDataSetTableAdapters.TaskDependenciesTableAdapter();
+            this.colId = new DevExpress.XtraScheduler.Native.ResourceTreeColumn();
+            this.colIdSort = new DevExpress.XtraScheduler.Native.ResourceTreeColumn();
+            this.colDescription = new DevExpress.XtraScheduler.Native.ResourceTreeColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1)).BeginInit();
+            this.splitContainerControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.resourcesTree1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerStorage1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.taskDependenciesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gantTestDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.appointmentsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resourcesBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainerControl1
+            // 
+            this.splitContainerControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerControl1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainerControl1.Name = "splitContainerControl1";
+            this.splitContainerControl1.Panel1.Controls.Add(this.resourcesTree1);
+            this.splitContainerControl1.Panel1.Text = "Panel1";
+            this.splitContainerControl1.Panel2.Controls.Add(this.schedulerControl1);
+            this.splitContainerControl1.Panel2.Text = "Panel2";
+            this.splitContainerControl1.Size = new System.Drawing.Size(700, 489);
+            this.splitContainerControl1.SplitterPosition = 179;
+            this.splitContainerControl1.TabIndex = 0;
+            this.splitContainerControl1.Text = "splitContainerControl1";
+            // 
+            // resourcesTree1
+            // 
+            this.resourcesTree1.Columns.AddRange(new DevExpress.XtraTreeList.Columns.TreeListColumn[] {
+            this.colIdSort,
+            this.colId,
+            this.colDescription});
+            this.resourcesTree1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.resourcesTree1.Location = new System.Drawing.Point(0, 0);
+            this.resourcesTree1.Name = "resourcesTree1";
+            this.resourcesTree1.OptionsBehavior.Editable = false;
+            this.resourcesTree1.SchedulerControl = this.schedulerControl1;
+            this.resourcesTree1.Size = new System.Drawing.Size(179, 489);
+            this.resourcesTree1.TabIndex = 0;
+            // 
+            // schedulerControl1
+            // 
+            this.schedulerControl1.DataStorage = this.schedulerStorage1;
+            this.schedulerControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.schedulerControl1.Location = new System.Drawing.Point(0, 0);
+            this.schedulerControl1.Name = "schedulerControl1";
+            this.schedulerControl1.Size = new System.Drawing.Size(509, 489);
+            this.schedulerControl1.Start = new System.DateTime(2017, 6, 27, 0, 0, 0, 0);
+            this.schedulerControl1.TabIndex = 0;
+            this.schedulerControl1.Text = "schedulerControl1";
+            this.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler4);
+            this.schedulerControl1.Views.FullWeekView.Enabled = true;
+            this.schedulerControl1.Views.FullWeekView.TimeRulers.Add(timeRuler5);
+            this.schedulerControl1.Views.WeekView.Enabled = false;
+            this.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler6);
+            // 
+            // schedulerStorage1
+            // 
+            this.schedulerStorage1.AppointmentDependencies.DataSource = this.taskDependenciesBindingSource;
+            this.schedulerStorage1.AppointmentDependencies.Mappings.DependentId = "DependentId";
+            this.schedulerStorage1.AppointmentDependencies.Mappings.ParentId = "ParentId";
+            this.schedulerStorage1.AppointmentDependencies.Mappings.Type = "Type";
+            this.schedulerStorage1.Appointments.DataSource = this.appointmentsBindingSource;
+            this.schedulerStorage1.Appointments.Mappings.AllDay = "AllDay";
+            this.schedulerStorage1.Appointments.Mappings.AppointmentId = "UniqueId";
+            this.schedulerStorage1.Appointments.Mappings.Description = "Description";
+            this.schedulerStorage1.Appointments.Mappings.End = "EndDate";
+            this.schedulerStorage1.Appointments.Mappings.Label = "Label";
+            this.schedulerStorage1.Appointments.Mappings.Location = "Location";
+            this.schedulerStorage1.Appointments.Mappings.PercentComplete = "PercentComplete";
+            this.schedulerStorage1.Appointments.Mappings.RecurrenceInfo = "RecurrenceInfo";
+            this.schedulerStorage1.Appointments.Mappings.ReminderInfo = "ReminderInfo";
+            this.schedulerStorage1.Appointments.Mappings.ResourceId = "ResourceId";
+            this.schedulerStorage1.Appointments.Mappings.Start = "StartDate";
+            this.schedulerStorage1.Appointments.Mappings.Status = "Status";
+            this.schedulerStorage1.Appointments.Mappings.Subject = "Subject";
+            this.schedulerStorage1.Appointments.Mappings.TimeZoneId = "TimeZoneId";
+            this.schedulerStorage1.Appointments.Mappings.Type = "Type";
+            this.schedulerStorage1.Resources.CustomFieldMappings.Add(new DevExpress.XtraScheduler.ResourceCustomFieldMapping("IdSort", "IdSort"));
+            this.schedulerStorage1.Resources.CustomFieldMappings.Add(new DevExpress.XtraScheduler.ResourceCustomFieldMapping("CustomField1", "CustomField1"));
+            this.schedulerStorage1.Resources.DataSource = this.resourcesBindingSource;
+            this.schedulerStorage1.Resources.Mappings.Caption = "Description";
+            this.schedulerStorage1.Resources.Mappings.Color = "Color";
+            this.schedulerStorage1.Resources.Mappings.Id = "Id";
+            this.schedulerStorage1.Resources.Mappings.Image = "Image";
+            this.schedulerStorage1.Resources.Mappings.ParentId = "ParentId";
+            // 
+            // taskDependenciesBindingSource
+            // 
+            this.taskDependenciesBindingSource.DataMember = "TaskDependencies";
+            this.taskDependenciesBindingSource.DataSource = this.gantTestDataSet;
+            // 
+            // gantTestDataSet
+            // 
+            this.gantTestDataSet.DataSetName = "GantTestDataSet";
+            this.gantTestDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // appointmentsBindingSource
+            // 
+            this.appointmentsBindingSource.DataMember = "Appointments";
+            this.appointmentsBindingSource.DataSource = this.gantTestDataSet;
+            // 
+            // resourcesBindingSource
+            // 
+            this.resourcesBindingSource.DataMember = "Resources";
+            this.resourcesBindingSource.DataSource = this.gantTestDataSet;
+            // 
+            // appointmentsTableAdapter
+            // 
+            this.appointmentsTableAdapter.ClearBeforeFill = true;
+            // 
+            // resourcesTableAdapter
+            // 
+            this.resourcesTableAdapter.ClearBeforeFill = true;
+            // 
+            // taskDependenciesTableAdapter
+            // 
+            this.taskDependenciesTableAdapter.ClearBeforeFill = true;
+            // 
+            // colId
+            // 
+            this.colId.FieldName = "Id";
+            this.colId.Name = "colId";
+            // 
+            // colIdSort
+            // 
+            this.colIdSort.FieldName = "IdSort";
+            this.colIdSort.Name = "colIdSort";
+            this.colIdSort.SortOrder = System.Windows.Forms.SortOrder.Ascending;
+            // 
+            // colDescription
+            // 
+            this.colDescription.FieldName = "Description";
+            this.colDescription.Name = "colDescription";
+            this.colDescription.Visible = true;
+            this.colDescription.VisibleIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 489);
+            this.Controls.Add(this.splitContainerControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1)).EndInit();
+            this.splitContainerControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.resourcesTree1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerStorage1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.taskDependenciesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gantTestDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.appointmentsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resourcesBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SplitContainerControl splitContainerControl1;
+        private DevExpress.XtraScheduler.SchedulerControl schedulerControl1;
+        private DevExpress.XtraScheduler.SchedulerStorage schedulerStorage1;
+        private GantTestDataSet gantTestDataSet;
+        private System.Windows.Forms.BindingSource appointmentsBindingSource;
+        private GantTestDataSetTableAdapters.AppointmentsTableAdapter appointmentsTableAdapter;
+        private System.Windows.Forms.BindingSource resourcesBindingSource;
+        private GantTestDataSetTableAdapters.ResourcesTableAdapter resourcesTableAdapter;
+        private System.Windows.Forms.BindingSource taskDependenciesBindingSource;
+        private GantTestDataSetTableAdapters.TaskDependenciesTableAdapter taskDependenciesTableAdapter;
+        private DevExpress.XtraScheduler.UI.ResourcesTree resourcesTree1;
+        private DevExpress.XtraScheduler.Native.ResourceTreeColumn colIdSort;
+        private DevExpress.XtraScheduler.Native.ResourceTreeColumn colId;
+        private DevExpress.XtraScheduler.Native.ResourceTreeColumn colDescription;
+    }
+}
+
---
+++ VB/GanttExample/Form1.Designer.vb
@@ -0,0 +1,216 @@
+﻿Namespace GanttExample
+    Partial Public Class Form1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        #Region "Windows Form Designer generated code"
+
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Dim timeRuler4 As New DevExpress.XtraScheduler.TimeRuler()
+            Dim timeRuler5 As New DevExpress.XtraScheduler.TimeRuler()
+            Dim timeRuler6 As New DevExpress.XtraScheduler.TimeRuler()
+            Me.splitContainerControl1 = New DevExpress.XtraEditors.SplitContainerControl()
+            Me.resourcesTree1 = New DevExpress.XtraScheduler.UI.ResourcesTree()
+            Me.schedulerControl1 = New DevExpress.XtraScheduler.SchedulerControl()
+            Me.schedulerStorage1 = New DevExpress.XtraScheduler.SchedulerStorage(Me.components)
+            Me.taskDependenciesBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+            Me.gantTestDataSet = New GantTestDataSet()
+            Me.appointmentsBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+            Me.resourcesBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+            Me.appointmentsTableAdapter = New GantTestDataSetTableAdapters.AppointmentsTableAdapter()
+            Me.resourcesTableAdapter = New GantTestDataSetTableAdapters.ResourcesTableAdapter()
+            Me.taskDependenciesTableAdapter = New GantTestDataSetTableAdapters.TaskDependenciesTableAdapter()
+            Me.colId = New DevExpress.XtraScheduler.Native.ResourceTreeColumn()
+            Me.colIdSort = New DevExpress.XtraScheduler.Native.ResourceTreeColumn()
+            Me.colDescription = New DevExpress.XtraScheduler.Native.ResourceTreeColumn()
+            CType(Me.splitContainerControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.splitContainerControl1.SuspendLayout()
+            CType(Me.resourcesTree1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.schedulerStorage1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.taskDependenciesBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.gantTestDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.appointmentsBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.resourcesBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' splitContainerControl1
+            ' 
+            Me.splitContainerControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.splitContainerControl1.Location = New System.Drawing.Point(0, 0)
+            Me.splitContainerControl1.Name = "splitContainerControl1"
+            Me.splitContainerControl1.Panel1.Controls.Add(Me.resourcesTree1)
+            Me.splitContainerControl1.Panel1.Text = "Panel1"
+            Me.splitContainerControl1.Panel2.Controls.Add(Me.schedulerControl1)
+            Me.splitContainerControl1.Panel2.Text = "Panel2"
+            Me.splitContainerControl1.Size = New System.Drawing.Size(700, 489)
+            Me.splitContainerControl1.SplitterPosition = 179
+            Me.splitContainerControl1.TabIndex = 0
+            Me.splitContainerControl1.Text = "splitContainerControl1"
+            ' 
+            ' resourcesTree1
+            ' 
+            Me.resourcesTree1.Columns.AddRange(New DevExpress.XtraTreeList.Columns.TreeListColumn() { Me.colIdSort, Me.colId, Me.colDescription})
+            Me.resourcesTree1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.resourcesTree1.Location = New System.Drawing.Point(0, 0)
+            Me.resourcesTree1.Name = "resourcesTree1"
+            Me.resourcesTree1.OptionsBehavior.Editable = False
+            Me.resourcesTree1.SchedulerControl = Me.schedulerControl1
+            Me.resourcesTree1.Size = New System.Drawing.Size(179, 489)
+            Me.resourcesTree1.TabIndex = 0
+            ' 
+            ' schedulerControl1
+            ' 
+            Me.schedulerControl1.DataStorage = Me.schedulerStorage1
+            Me.schedulerControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.schedulerControl1.Location = New System.Drawing.Point(0, 0)
+            Me.schedulerControl1.Name = "schedulerControl1"
+            Me.schedulerControl1.Size = New System.Drawing.Size(509, 489)
+            Me.schedulerControl1.Start = New Date(2017, 6, 27, 0, 0, 0, 0)
+            Me.schedulerControl1.TabIndex = 0
+            Me.schedulerControl1.Text = "schedulerControl1"
+            Me.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler4)
+            Me.schedulerControl1.Views.FullWeekView.Enabled = True
+            Me.schedulerControl1.Views.FullWeekView.TimeRulers.Add(timeRuler5)
+            Me.schedulerControl1.Views.WeekView.Enabled = False
+            Me.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler6)
+            ' 
+            ' schedulerStorage1
+            ' 
+            Me.schedulerStorage1.AppointmentDependencies.DataSource = Me.taskDependenciesBindingSource
+            Me.schedulerStorage1.AppointmentDependencies.Mappings.DependentId = "DependentId"
+            Me.schedulerStorage1.AppointmentDependencies.Mappings.ParentId = "ParentId"
+            Me.schedulerStorage1.AppointmentDependencies.Mappings.Type = "Type"
+            Me.schedulerStorage1.Appointments.DataSource = Me.appointmentsBindingSource
+            Me.schedulerStorage1.Appointments.Mappings.AllDay = "AllDay"
+            Me.schedulerStorage1.Appointments.Mappings.AppointmentId = "UniqueId"
+            Me.schedulerStorage1.Appointments.Mappings.Description = "Description"
+            Me.schedulerStorage1.Appointments.Mappings.End = "EndDate"
+            Me.schedulerStorage1.Appointments.Mappings.Label = "Label"
+            Me.schedulerStorage1.Appointments.Mappings.Location = "Location"
+            Me.schedulerStorage1.Appointments.Mappings.PercentComplete = "PercentComplete"
+            Me.schedulerStorage1.Appointments.Mappings.RecurrenceInfo = "RecurrenceInfo"
+            Me.schedulerStorage1.Appointments.Mappings.ReminderInfo = "ReminderInfo"
+            Me.schedulerStorage1.Appointments.Mappings.ResourceId = "ResourceId"
+            Me.schedulerStorage1.Appointments.Mappings.Start = "StartDate"
+            Me.schedulerStorage1.Appointments.Mappings.Status = "Status"
+            Me.schedulerStorage1.Appointments.Mappings.Subject = "Subject"
+            Me.schedulerStorage1.Appointments.Mappings.TimeZoneId = "TimeZoneId"
+            Me.schedulerStorage1.Appointments.Mappings.Type = "Type"
+            Me.schedulerStorage1.Resources.CustomFieldMappings.Add(New DevExpress.XtraScheduler.ResourceCustomFieldMapping("IdSort", "IdSort"))
+            Me.schedulerStorage1.Resources.CustomFieldMappings.Add(New DevExpress.XtraScheduler.ResourceCustomFieldMapping("CustomField1", "CustomField1"))
+            Me.schedulerStorage1.Resources.DataSource = Me.resourcesBindingSource
+            Me.schedulerStorage1.Resources.Mappings.Caption = "Description"
+            Me.schedulerStorage1.Resources.Mappings.Color = "Color"
+            Me.schedulerStorage1.Resources.Mappings.Id = "Id"
+            Me.schedulerStorage1.Resources.Mappings.Image = "Image"
+            Me.schedulerStorage1.Resources.Mappings.ParentId = "ParentId"
+            ' 
+            ' taskDependenciesBindingSource
+            ' 
+            Me.taskDependenciesBindingSource.DataMember = "TaskDependencies"
+            Me.taskDependenciesBindingSource.DataSource = Me.gantTestDataSet
+            ' 
+            ' gantTestDataSet
+            ' 
+            Me.gantTestDataSet.DataSetName = "GantTestDataSet"
+            Me.gantTestDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+            ' 
+            ' appointmentsBindingSource
+            ' 
+            Me.appointmentsBindingSource.DataMember = "Appointments"
+            Me.appointmentsBindingSource.DataSource = Me.gantTestDataSet
+            ' 
+            ' resourcesBindingSource
+            ' 
+            Me.resourcesBindingSource.DataMember = "Resources"
+            Me.resourcesBindingSource.DataSource = Me.gantTestDataSet
+            ' 
+            ' appointmentsTableAdapter
+            ' 
+            Me.appointmentsTableAdapter.ClearBeforeFill = True
+            ' 
+            ' resourcesTableAdapter
+            ' 
+            Me.resourcesTableAdapter.ClearBeforeFill = True
+            ' 
+            ' taskDependenciesTableAdapter
+            ' 
+            Me.taskDependenciesTableAdapter.ClearBeforeFill = True
+            ' 
+            ' colId
+            ' 
+            Me.colId.FieldName = "Id"
+            Me.colId.Name = "colId"
+            ' 
+            ' colIdSort
+            ' 
+            Me.colIdSort.FieldName = "IdSort"
+            Me.colIdSort.Name = "colIdSort"
+            Me.colIdSort.SortOrder = System.Windows.Forms.SortOrder.Ascending
+            ' 
+            ' colDescription
+            ' 
+            Me.colDescription.FieldName = "Description"
+            Me.colDescription.Name = "colDescription"
+            Me.colDescription.Visible = True
+            Me.colDescription.VisibleIndex = 0
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(700, 489)
+            Me.Controls.Add(Me.splitContainerControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType(Me.splitContainerControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.splitContainerControl1.ResumeLayout(False)
+            CType(Me.resourcesTree1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.schedulerStorage1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.taskDependenciesBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.gantTestDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.appointmentsBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.resourcesBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+
+        End Sub
+
+        #End Region
+
+        Private splitContainerControl1 As DevExpress.XtraEditors.SplitContainerControl
+        Private schedulerControl1 As DevExpress.XtraScheduler.SchedulerControl
+        Private schedulerStorage1 As DevExpress.XtraScheduler.SchedulerStorage
+        Private gantTestDataSet As GantTestDataSet
+        Private appointmentsBindingSource As System.Windows.Forms.BindingSource
+        Private appointmentsTableAdapter As GantTestDataSetTableAdapters.AppointmentsTableAdapter
+        Private resourcesBindingSource As System.Windows.Forms.BindingSource
+        Private resourcesTableAdapter As GantTestDataSetTableAdapters.ResourcesTableAdapter
+        Private taskDependenciesBindingSource As System.Windows.Forms.BindingSource
+        Private taskDependenciesTableAdapter As GantTestDataSetTableAdapters.TaskDependenciesTableAdapter
+        Private resourcesTree1 As DevExpress.XtraScheduler.UI.ResourcesTree
+        Private colIdSort As DevExpress.XtraScheduler.Native.ResourceTreeColumn
+        Private colId As DevExpress.XtraScheduler.Native.ResourceTreeColumn
+        Private colDescription As DevExpress.XtraScheduler.Native.ResourceTreeColumn
+    End Class
+End Namespace
+
